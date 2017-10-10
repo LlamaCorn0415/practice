469f7794efbecd423f7b13f8a62fe1109883739d
LlamaCorn0415 <guineapig4444@gmail.com>
2017-10-09 20:20:40 -0700

    Project

---
+++ while loop/src/HelloWorld.kt
@@ -0,0 +1,28 @@
+
+
+fun main(args: Array<String>) {
+
+    // Value assigned to variable
+    var meow = 10
+
+
+
+    //Telling the program what to do while meow is not equal to 0
+    while (meow >= 0 ) {
+        println("Nachos")
+        println(meow)
+        meow -= 3
+    }
+    //Tells program that if meow does equal 0 to print line or else print other line
+    if (meow == 0) {
+        println("YUM!")
+    } else print("munch!")
+//
+}
+
+
+
+
+
+
+ 
