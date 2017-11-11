d4512dd9d5bfd3eb5bb906783817cd4e703cddd8
LlamaCorn0415 <guineapig4444@gmail.com>
2017-11-10 20:12:40 -0800

    Practice for android

---
+++ Practice/app/src/main/java/com/example/hannahroseneill/practice/FloofyActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hannahroseneill.practice
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by hannahroseneill on 11/10/17.
+ */
+class FloofyActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.nextscreen)
+	}
+}
---
+++ Practice/app/src/main/java/com/example/hannahroseneill/practice/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.hannahroseneill.practice
+
+import android.content.Intent
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.sql.ResultSet
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+		switch1.setOnCheckedChangeListener { buttonView, isChecked ->
+			if (isChecked) {
+				textView.text = "Awesome Sauce!"
+				imageView.setImageResource(R.drawable.happycat)
+				//background.setBackgroundColor(Color.YELLOW)
+				background.setBackgroundResource(R.color.happy)
+			} else {
+				textView.text = "Not awesome sauce"
+				imageView.setImageResource(R.drawable.sadcat)
+				//background.setBackgroundColor(Color.DKGRAY)
+				background.setBackgroundResource(R.color.sad)
+
+			}
+		}
+		floofybutton.setOnClickListener {
+			var intent = Intent(this,FloofyActivity::class.java)
+			startActivity(intent)
+		} 
+	}
+}
